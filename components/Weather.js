394544ac149ce482179d0bbbe4e411dd8b85d108
VictoriaStudios <88744359+VictoriaStudios@users.noreply.github.com>
2022-02-22 13:40:09 -0500

    Further work on the style completed

--- components/Weather.js
+++ components/Weather.js
@@ -14,15 +14,14 @@ function Weather(props) {
           <div className="innerWrapper">
             <div className="weatherSymbol">
               <WeatherSymbol dayWeather={data.current} imgWidth={164} imgHeight={164}/>
+              <h3>{props.locationData.locality}, {props.locationData.countryName}</h3>
             </div>
             <div className="basicInfos">
               <h3>{useToCelsius(data.current.temp)}°C</h3>
               <h3>{data.current.humidity}% humidity</h3>
               <h3>{useIdToWeatherString(data.current.weather[0].id)}</h3>
             </div>
-            <div className="secondRow">
-              <h3>{props.locationData.locality}, {props.locationData.countryName}</h3>
-            </div>
+            <div className="gap"/>
             <div className="thirdRow">
               {(data.daily.slice(1, 4)).map((entry, index) => (
                 <div key={`forecast${index}`} className="forecastContainer"><Forecast dayWeather={entry} imgWidth={64} imgHeight={64} index={index}/></div>
